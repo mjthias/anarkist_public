15dc03cbddae7b9a56897f8bcc2775124a04be63
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2023-06-02 13:44:13 +0200

    AB-156 created navigation styling

--- public/main.js
+++ public/main.js
@@ -2,3 +2,15 @@ function toggleReadMoreAccordion() {
     const accordion = document.querySelector(`[data-accordion="${event.target.dataset.target}"]`);
     accordion.classList.toggle("max-h-104");
 }
+
+function toggleBurgerMenu() {
+    const burger = document.querySelector("#burger");
+    const burgerClose = document.querySelector("#burger-close");
+    burger.classList.toggle("rotate-180");
+    burgerClose.classList.toggle("rotate-180");
+    burger.querySelector("svg").classList.toggle("opacity-0");
+    burgerClose.querySelector("svg").classList.toggle("opacity-0");
+
+    const menu = document.querySelector(".burger-menu");
+    menu.classList.toggle("!translate-x-0");
+}
